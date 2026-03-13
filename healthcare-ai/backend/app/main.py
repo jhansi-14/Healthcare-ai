6fa5eaae14cf9aaf57094d93140846d71f745470
jhansi-14 <jhansireddypuli@gmail.com>
2026-03-13 22:09:23 +0530

    adding databases

--- healthcare-ai/backend/app/main.py
+++ healthcare-ai/backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from backend.app.routers.user_router import router
 from backend.app.agents.agent_router import router as agent_router
+from backend.app.database.models import Base
+from backend.app.database.db import engine
+from backend.app.routers.health_router import router as health_router
 app=FastAPI()    
+Base.metadata.create_all(bind=engine)
 @app.get("/")
 def home():
     return{"message":"Healthcare Agent Running"} 
 app.include_router(router) 
-app.include_router(agent_router)         
+app.include_router(agent_router) 
+app.include_router(health_router)        
