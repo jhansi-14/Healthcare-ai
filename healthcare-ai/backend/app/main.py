d0fd64e9dc511f43614cc56908b10ba35e05ca4a
jhansi-14 <jhansireddypuli@gmail.com>
2026-03-13 20:23:18 +0530

    adding role based authentication

--- healthcare-ai/backend/app/main.py
+++ healthcare-ai/backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from backend.app.routers import user_router
 app=FastAPI()
+app.include_router(user_router.router)    
 @app.get("/")
 def home():
-    return{"message":"Healthcare Agent Running"}
+    return{"message":"Healthcare Agent Running"}   
---
+++ healthcare-ai/backend/app/routers/user_router.py
@@ -0,0 +1,7 @@
+from fast import APIRouter
+from backend.app.auth.auth_handler importcreate_token
+router=APIRouter()
+@router.get("/login")
+def login():
+    token=create_token({"user":"john","role":"doctor"})
+    return{"token":token}
