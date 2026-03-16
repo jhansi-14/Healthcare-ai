d058b9d35cec5a1993e13aa02a90c60db9e3f235
jhansi-14 <jhansireddypuli@gmail.com>
2026-03-16 21:04:07 +0530

    fix indendation error

--- healthcare-ai/backend/app/routers/health_router.py
+++ healthcare-ai/backend/app/routers/health_router.py
@@ -2,20 +2,25 @@ from fastapi import APIRouter
 from backend.app.database.db import SessionLocal
 from backend.app.database.models import HealthMetrics
 import logging
-router=APIRouter()
-logger=logging.getLogger(__name__)
+
+router = APIRouter()
+logger = logging.getLogger(_name_)
+
 @router.post("/health")
-def save_health(patient_id:int,heart_rate:int,steps:int,sleep_hours:float):
+def save_health(patient_id: int, heart_rate: int, steps: int, sleep_hours: float):
     logger.info(f"Recieved health data for patient {patient_id}")
-    db=SessionLocal()
-    record=HealthMetrics(
+
+    db = SessionLocal()
+
+    record = HealthMetrics(
         patient_id=patient_id,
         heart_rate=heart_rate,
         steps=steps,
         sleep_hours=sleep_hours
     )
+
     db.add(record)
     db.commit()
     db.close()
-    logger.info("Health data saved succesfully")
-    return {"message":"Health data saved"}
+
+    logger.info("Health data saved successfully")
